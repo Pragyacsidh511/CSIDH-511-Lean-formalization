3b3001b7661ee12c72e67cc468245f2b72e9462c
Pragyacsidh511 <kpragya056@gmail.com>
2026-05-31 20:46:46 +0530

    Update Csidh511.lean

--- Csidh511.lean
+++ Csidh511.lean
@@ -4,3 +4,8 @@ def d : Int := -511
 #eval d + 511
 def security_bits : Nat := 128 
 #eval security_bits
+
+/--Step 3: if d = -511 then security is 128 -/
+theorem security_of_d : d = -511 \to security_bits = 128 :=by intro h 
+rw [h]
+rfl
